65eb37971cf95971edfdd743700364d7284e4375
SR Joy <salim15-3117@diu.edu.bd>
2021-09-01 03:22:30 +0600

    google clone project

---
+++ src/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = "AIzaSyCCqbdTlgSlqQmCKXZ8caY41V8c-dbpk3Q";
+export default API_KEY;
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import API_KEY from "./keys";
+
+const CONTEXT_KEY = "eaced19ce01eb58bc";
+
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+
+  return { data };
+};
+
+export default useGoogleSearch;
